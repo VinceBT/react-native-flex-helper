065caf66ca39f91b68f4c7285d7777c9d6981a94
VinceBT <vincebt06@gmail.com>
2016-11-28 13:22:06 +0100

    Added somes rules + V1.2.0

--- index.js
+++ index.js
@@ -3,6 +3,98 @@
  * https://github.com/VinceBT/react-native-flex-helper
  */
 
-const FlexHelper = require('./FlexHelper');
-
-module.exports = FlexHelper;
+module.exports = require('react-native').StyleSheet.create({
+    fill: {
+        flex: 1
+    },
+    center: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    column: {
+        flexDirection: 'column'
+    },
+    row: {
+        flexDirection: 'row'
+    },
+    mainStart: {
+        justifyContent: 'flex-start'
+    },
+    mainCenter: {
+        justifyContent: 'center'
+    },
+    mainEnd: {
+        justifyContent: 'flex-end'
+    },
+    mainSpaceBetween: {
+        justifyContent: 'space-between'
+    },
+    mainSpaceAround: {
+        justifyContent: 'space-around'
+    },
+    crossStart: {
+        alignItems: 'flex-start'
+    },
+    crossCenter: {
+        alignItems: 'center'
+    },
+    crossEnd: {
+        alignItems: 'flex-end'
+    },
+    crossStretch: {
+        alignItems: 'stretch'
+    },
+    colMain: {
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    colCross: {
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    rowMain: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    rowCross: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    relative: {
+        position: 'relative'
+    },
+    absolute: {
+        position: 'absolute'
+    },
+    absoluteFill: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
+    },
+    absoluteTop: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0
+    },
+    absoluteLeft: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0
+    },
+    absoluteRight: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0
+    },
+    absoluteBottom: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0
+    }
+});
